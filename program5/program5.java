198150ec2b0ca6d6225ccd55aee6a6f61ca8b399
Vismay05 <01fe23bca150@kletech.ac.in>
2025-05-28 11:17:15 +0530

    Add program5.java with Fibonacci series implementation

---
+++ program5/program5.java
@@ -0,0 +1,19 @@
+package program5;
+
+public class program5 {
+        public static void main(String[] args) { 
+        int n = 10; // number of terms to generate 
+  
+        int a = 0, b = 1, c; 
+  
+        System.out.println("Fibonacci Series up to " + n + " terms:"); 
+        for (int i = 0; i < n; i++) { 
+            System.out.print(a + " "); 
+            c = a + b; 
+            a = b; 
+            b = c; 
+        } 
+    } 
+} 
+    
+
